500d4e49a4ae482b1942c15ac9222b2b78e7a013
GoldenAnpu <seryios.sych@gmail.com>
2025-05-09 18:46:51 +0200

    Update Docker image version to 0.1.3rc4 in config.json and build_push.sh

--- docker/build_push.sh
+++ docker/build_push.sh
@@ -1,4 +1,4 @@
 cp ../dev_requirements.txt . && \
-docker build --no-cache -t supervisely/embeddings-generator:0.1.2 . && \
+docker build --no-cache -t supervisely/embeddings-generator:0.1.3rc4 . && \
 rm dev_requirements.txt && \
-docker push supervisely/embeddings-generator:0.1.2 
+docker push supervisely/embeddings-generator:0.1.3rc4 
